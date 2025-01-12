741fa014be2c458b5a40bc7f069ef510b95a2c8b
Andre Moreno <andremoreno@users.noreply.github.com>
2025-01-12 18:18:01 +0700

    Update AppFooter.vue

--- app/components/AppFooter.vue
+++ app/components/AppFooter.vue
@@ -29,7 +29,18 @@ const isDark = computed({
             <div class="w-8 h-8" />
           </template>
         </ClientOnly>
-        
+        <UButton
+          to="https://git.new/draw"
+          color="gray"
+          variant="ghost"
+          icon="i-simple-icons-github"
+        />
+        <UButton
+          to="https://x.com/atinux"
+          color="gray"
+          variant="ghost"
+          icon="i-simple-icons-x"
+        />
       </div>
       <div class="flex items-center justify-center lg:justify-start lg:flex-1 gap-x-1.5 mt-3 lg:mt-0 lg:order-1">
         <span class="text-sm">Made with <UButton
